7b62b7d8f34702a3df68da7a450c0cd60afab170
Martin Mallea <martin.mallea@wolox.com.ar>
2020-04-30 12:41:26 -0300

    Hashing password when creating an user

--- app/services/users.js
+++ app/services/users.js
@@ -1,10 +1,19 @@
 const { ValidationError } = require('sequelize');
+const { hash } = require('bcrypt');
 
 const { users } = require('../models');
-const errors = require('../errors');
+const { sequelizeValidationError } = require('../errors');
 
-exports.create = params =>
-  users.create(params).catch(error => {
-    if (error instanceof ValidationError) throw errors.sequelizeValidationError(error);
-    throw error;
-  });
+const withHashedPassword = userParams => {
+  const { password } = userParams;
+  const saltRounds = 10;
+  return hash(password, saltRounds).then(hashedPassword => ({ ...userParams, password: hashedPassword }));
+};
+
+exports.create = userParams =>
+  withHashedPassword(userParams)
+    .then(user => users.create(user))
+    .catch(error => {
+      if (error instanceof ValidationError) throw sequelizeValidationError(error);
+      throw error;
+    });
