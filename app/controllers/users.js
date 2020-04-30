ba010fd3651be18019161dfcd4936eb6fefde001
Martin Mallea <martin.mallea@wolox.com.ar>
2020-04-30 15:04:22 -0300

    Fixed error messages

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -6,16 +6,16 @@ const { creationsParamsMapper } = require('../mappers/users');
 const { showSerializer } = require('../serializers/users');
 
 exports.create = [
-  body('user.first_name', 'firstName must be present')
+  body('user.first_name', 'first_name must be present')
     .not()
     .isEmpty(),
-  body('user.last_name', 'lastName must be present')
+  body('user.last_name', 'last_name must be present')
     .not()
     .isEmpty(),
   body('user.password', 'password must be present')
     .not()
     .isEmpty(),
-  body('user.email', 'email must be an email').isEmail(),
+  body('user.email', 'email must have email format').isEmail(),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) throw fieldValidationError(errors.mapped());
