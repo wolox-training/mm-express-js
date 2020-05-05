aaffd22db43a8e97bdc051e92f43e018f17d02a8
Martin Mallea <martin.mallea@wolox.com.ar>
2020-05-05 15:06:32 -0300

    Snake case file names

--- app/mappers/users.js
+++ app/mappers/users.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-const { deepCamelizeKeys } = require('../helpers/objectUtils');
+const { deepCamelizeKeys } = require('../helpers/object_utils');
 
 exports.creationParamsMapper = params => {
   const filteredParams = _.pick(params, 'first_name', 'last_name', 'password', 'email');
