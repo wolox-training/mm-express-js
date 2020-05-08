8247c917121ae5feb78a534c58e9677b348b5989
Martin Mallea <martin.mallea@wolox.com.ar>
2020-05-08 15:41:08 -0300

    Fixed POST /users/sessions specs

--- test/sessions/post.spec.js
+++ test/sessions/post.spec.js
@@ -5,20 +5,26 @@ const { hashPassword } = require('../../app/helpers/passwords');
 const { createUser } = require('../factory/users_factory');
 const { FIELD_VALIDATION_ERROR, INVALID_LOGIN_ERROR } = require('../../app/errors');
 const { decode } = require('../../app/helpers/jwt_utils');
+const { truncateDatabase } = require('../utils');
 
 describe('POST /users/sessions', () => {
   const httpRequest = params =>
     request(app)
       .post('/users/sessions')
       .send(params);
+  let sessionCreationResponse = {};
 
   describe('with an user created', () => {
     const email = 'user_email@wolox.co';
     const password = 'secret_password_123';
-    beforeEach(async () => createUser({ email, password: await hashPassword(password) }));
+    beforeAll(async () => createUser({ email, password: await hashPassword(password) }));
+    afterAll(() => truncateDatabase());
 
     describe('when sending the correct params', () => {
       const params = { email, password };
+      beforeAll(async () => {
+        sessionCreationResponse = await httpRequest(params);
+      });
 
       test('Responds with 201 created', () =>
         httpRequest(params).then(response => expect(response.status).toBe(201)));
@@ -34,34 +40,38 @@ describe('POST /users/sessions', () => {
 
     describe('when sending an invalid password', () => {
       const params = { email, password: 'invalid' };
+      beforeAll(async () => {
+        sessionCreationResponse = await httpRequest(params);
+      });
 
-      test('Responds with 422 unprocessable entity', () =>
-        httpRequest(params).then(response => expect(response.status).toBe(422)));
+      test('Responds with 422 unprocessable entity', () => expect(sessionCreationResponse.status).toBe(422));
 
       test('Responds with the expected error code', () =>
-        httpRequest(params).then(response => expect(response.body.internal_code).toBe(INVALID_LOGIN_ERROR)));
+        expect(sessionCreationResponse.body.internal_code).toBe(INVALID_LOGIN_ERROR));
     });
 
     describe('with invalid params', () => {
       const params = { email: 'afdsaafsd' };
+      beforeAll(async () => {
+        sessionCreationResponse = await httpRequest(params);
+      });
 
-      test('Responds with 422 unprocessable entity', () =>
-        httpRequest(params).then(response => expect(response.status).toBe(422)));
+      test('Responds with 422 unprocessable entity', () => expect(sessionCreationResponse.status).toBe(422));
 
       test('Responds with the expected error code', () =>
-        httpRequest(params).then(response =>
-          expect(response.body.internal_code).toBe(FIELD_VALIDATION_ERROR)
-        ));
+        expect(sessionCreationResponse.body.internal_code).toBe(FIELD_VALIDATION_ERROR));
     });
   });
 
   describe('without an user created', () => {
     const params = { email: 'tincho@wolox.co', password: 'wolotz1189' };
+    beforeAll(async () => {
+      sessionCreationResponse = await httpRequest(params);
+    });
 
-    test('Responds with 422 unprocessable entity', () =>
-      httpRequest(params).then(response => expect(response.status).toBe(422)));
+    test('Responds with 422 unprocessable entity', () => expect(sessionCreationResponse.status).toBe(422));
 
     test('Responds with the expected error code', () =>
-      httpRequest(params).then(response => expect(response.body.internal_code).toBe(INVALID_LOGIN_ERROR)));
+      expect(sessionCreationResponse.body.internal_code).toBe(INVALID_LOGIN_ERROR));
   });
 });
