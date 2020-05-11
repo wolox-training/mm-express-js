d9f347046a94b44781651d4f6af7dd7adfd12d05
Martin Mallea <martin.mallea@wolox.com.ar>
2020-05-11 14:27:23 -0300

    Moved session schema to schemas/ folder

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,13 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const { createUser } = require('./controllers/users');
-const { validateUserEmailUniqueness } = require('./middlewares/users');
-const { userCreationSchema } = require('./schemas/users');
-const { schemaValidation } = require('./middlewares/fields_validation');
-const { sessionsBodyValidations } = require('./middlewares/sessions');
 const { createSession } = require('./controllers/sessions');
+const { validateUserEmailUniqueness } = require('./middlewares/users');
+const { schemaValidation } = require('./middlewares/fields_validation');
+const { userCreationSchema } = require('./schemas/users');
+const { sessionsCreationSchema } = require('./schemas/sessions');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.post('/users', [schemaValidation(userCreationSchema), validateUserEmailUniqueness], createUser);
-  app.post('/users/sessions', [sessionsBodyValidations], createSession);
+  app.post('/users/sessions', [schemaValidation(sessionsCreationSchema)], createSession);
 };
