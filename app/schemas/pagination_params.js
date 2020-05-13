a4607ff0361bc72b1e6acaa45a347773975d8118
Martin Mallea <martin.mallea@wolox.com.ar>
2020-05-13 12:48:18 -0300

    Added cases on users index specs

--- app/schemas/pagination_params.js
+++ app/schemas/pagination_params.js
@@ -5,12 +5,14 @@ exports.paginationParamsSchema = {
     in: ['query'],
     isInt: { options: { min: 1 } },
     toInt: true,
+    optional: true,
     errorMessage: 'page should be a positive integer'
   },
   limit: {
     in: ['query'],
     isInt: { options: { min: 1, max: maxPageSize } },
     toInt: true,
+    optional: true,
     errorMessage: 'limit should be a positive integer'
   }
 };
