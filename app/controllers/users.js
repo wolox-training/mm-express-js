8991db751733432efa462f4ce4acfe753a707abe
Martin Mallea <martin.mallea@wolox.com.ar>
2020-05-19 13:27:01 -0300

    Renamed userCreationEndpoint -> userCreation

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -4,7 +4,7 @@ const { paginationParamsMapper } = require('../mappers/pagination_params');
 const { showUserSerializer, usersPageSerializer } = require('../serializers/users');
 const { hashPassword } = require('../helpers/passwords');
 
-const userCreationEndpoint = creator => (req, res, next) => {
+const userCreation = creator => (req, res, next) => {
   const userBody = creationParamsMapper(req.body);
   return hashPassword(userBody.password)
     .then(password => creator({ ...userBody, password }))
@@ -12,9 +12,9 @@ const userCreationEndpoint = creator => (req, res, next) => {
     .catch(next);
 };
 
-exports.createUser = userCreationEndpoint(createUser);
+exports.createUser = userCreation(createUser);
 
-exports.createAdminUser = userCreationEndpoint(createAdminUser);
+exports.createAdminUser = userCreation(createAdminUser);
 
 exports.usersIndex = (req, res, next) =>
   findAndCountAllUsers(paginationParamsMapper(req.query))
