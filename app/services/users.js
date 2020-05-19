456477dcd65a280d403e105bd594e854b3cf3e6e
Martin Mallea <martin.mallea@wolox.com.ar>
2020-05-19 13:04:54 -0300

    Added logs on users service and throwing databaseError when getting an error from database

--- app/services/users.js
+++ app/services/users.js
@@ -1,16 +1,37 @@
 const { User } = require('../models');
+const { databaseError } = require('../errors');
+const { info } = require('../logger');
 
-exports.createUser = userParams => User.create(userParams);
+exports.createUser = userParams => {
+  info('Calling users.createUser');
+  return User.create(userParams).catch(error => {
+    throw databaseError(error);
+  });
+};
 
-exports.findUserByEmail = email => User.findOne({ where: { email } });
+exports.findUserByEmail = email => {
+  info('Calling users.findUserByEmail');
+  return User.findOne({ where: { email } }).catch(error => {
+    throw databaseError(error);
+  });
+};
 
-exports.findAndCountAllUsers = ({ offset, limit }) =>
-  User.findAndCountAll({ offset, limit, order: [['id', 'asc']] });
+exports.findAndCountAllUsers = ({ offset, limit }) => {
+  info('Calling users.findAndCountAllUsers');
+  return User.findAndCountAll({ offset, limit, order: [['id', 'asc']] }).catch(error => {
+    throw databaseError(error);
+  });
+};
 
 exports.createAdminUser = userParams => {
+  info('Calling users.createAdminUser');
   const defaultParams = { ...userParams, role: 'admin' };
   return User.findOrCreate({
     where: { email: userParams.email },
     defaults: defaultParams
-  }).then(([user, created]) => (created ? user : user.update(defaultParams)));
+  })
+    .then(([user, created]) => (created ? user : user.update(defaultParams)))
+    .catch(error => {
+      throw databaseError(error);
+    });
 };
