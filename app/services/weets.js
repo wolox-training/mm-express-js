2d31fa39b9ab57308e61c5ce43b7e6992ff22c9f
Martin Mallea <martin.mallea@wolox.com.ar>
2020-05-19 13:41:54 -0300

    Added logs on weets service, and catching errors

--- app/services/weets.js
+++ app/services/weets.js
@@ -1,7 +1,10 @@
 const { Weet } = require('../models');
+const { info } = require('../logger');
+const { databaseError } = require('../errors');
 
 exports.createWeet = (content, user) => {
+  info('Calling weets.createWeet');
   const weet = Weet.build({ content });
   weet.setUser(user, { save: false });
-  return weet.save();
+  return weet.save().catch(error => databaseError(error));
 };
