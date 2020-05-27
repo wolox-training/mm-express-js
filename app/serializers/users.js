8db36a670e63e2b4e9b4c524b772eae518a46b4b
Martin Mallea <martin.mallea@wolox.com.ar>
2020-05-27 13:15:44 -0300

    Moved userJobPosition function to usersService

--- app/serializers/users.js
+++ app/serializers/users.js
@@ -2,19 +2,11 @@ const { pick } = require('lodash');
 
 const { underscoreKeys } = require('../helpers/object_utils');
 const { pageSerializer } = require('./pagination');
-
-const jobPosition = ({ points }) => {
-  if (points <= 5) return 'DEVELOPER';
-  if (points <= 9) return 'LEAD';
-  if (points <= 19) return 'TL';
-  if (points <= 29) return 'EM';
-  if (points <= 49) return 'HEAD';
-  return 'CEO';
-};
+const { userJobPosition } = require('../services/users');
 
 exports.showUserSerializer = user => {
   const underscoredUser = underscoreKeys(pick(user, 'id', 'firstName', 'lastName', 'email'));
-  return { ...underscoredUser, job_position: jobPosition(user) };
+  return { ...underscoredUser, job_position: userJobPosition(user) };
 };
 
 exports.usersPageSerializer = pageSerializer(exports.showUserSerializer);
