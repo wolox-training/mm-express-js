3b77192506f285f9d309800cf8ffd04578ed0da6
Martin Mallea <martin.mallea@wolox.com.ar>
2020-06-01 16:05:07 -0300

    Removed testing config for mailer

--- config/testing.js
+++ config/testing.js
@@ -10,13 +10,6 @@ exports.config = {
     },
     geekJokesApi: {
       baseUrl: process.env.GEEK_JOKES_API_URL || 'https://testing.geekJokes.com/api'
-    },
-    mailer: {
-      user: process.env.MAILER_USER || 'maileruser123',
-      pass: process.env.MAILER_PASS || 'supersecret456',
-      host: process.env.MAILER_HOST || 'smtp.example.email',
-      port: process.env.MAILER_PORT || 587,
-      from: process.env.MAILER_FROM_EMAIL || 'notifications@weeter.com'
     }
   }
 };
