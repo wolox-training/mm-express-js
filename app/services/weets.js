daa0658e91d0d5cfa928244e59f94e39fbe3f0e9
Martin Mallea <martin.mallea@wolox.com.ar>
2020-06-04 13:12:14 -0300

    Added weetsService.wordsCountByUser function

--- app/services/weets.js
+++ app/services/weets.js
@@ -1,4 +1,9 @@
-const { Weet, User } = require('../models');
+const {
+  Weet,
+  User,
+  Sequelize: { Op },
+  sequelize
+} = require('../models');
 const { info } = require('../logger');
 const { databaseError } = require('../errors');
 
@@ -20,5 +25,35 @@ exports.findWeetById = id => {
   info('Calling weets.findWeetById');
   return Weet.findByPk(id, { include: { model: User, as: 'user' } }).catch(error => {
     throw databaseError(error.message);
+  });
+};
+
+exports.wordsCountByUser = ({ startingDate, limit }) => {
+  info(`Calling weets.wordsCountByUser with startingDate=${startingDate}`);
+  return Weet.findAll({
+    where: {
+      createdAt: {
+        [Op.gte]: startingDate
+      }
+    },
+    attributes: [
+      [
+        sequelize.fn(
+          'sum',
+          sequelize.fn(
+            'array_length',
+            sequelize.fn('regexp_split_to_array', sequelize.col('content'), sequelize.literal("E'\\\\s+'")),
+            1
+          )
+        ),
+        'totalWordsCount'
+      ]
+    ],
+    include: { model: User, as: 'user', required: true },
+    group: ['user.id'],
+    order: [[sequelize.literal('"totalWordsCount"'), 'DESC']],
+    limit
+  }).catch(({ message }) => {
+    throw databaseError(message);
   });
 };
